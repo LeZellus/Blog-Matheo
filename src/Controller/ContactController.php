22d4cddf2ebfadb055096c10a9d94ffbbaab7745
Zeller <matheo.zeller@gmail.com>
2021-01-18 10:06:47 +0100

    feat(contact): add contact controller

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,74 +2,49 @@
 
 namespace App\Controller;
 
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
 
 class ContactController extends AbstractController
 {
-  public function contact($post)
-  {
-    $articles = $this->articleDAO->getLastArticles();
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index(Request $request, MailerInterface $mailer)
+    {
+        $form = $this->createForm(ContactType::class);
 
-    if ($post->get('submit')) {
+        $form->handleRequest($request);
 
-      $errors = $this->validation->validate($post, 'Contact');
 
-      if (!$errors) {
+        if($form->isSubmitted() && $form->isValid()) {
 
-        $mail = new PHPMailer(true);
-        $EmailReceive = "matheo.zeller@gmail.com";
-        $EmailSend = "xipelgame@gmail.com";
+            $contactFormData = $form->getData();
 
-        $fromName = $post->get('name');
-        $fromEmail = $post->get('email');
-        $message = $post->get('message');
-        $subject = "Un visiteur du blog vous contacte";
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('matheo.zeller@gmail.com')
+                ->subject('You got mail')
+                ->text('Sender : '.$contactFormData['email'].\PHP_EOL.
+                    $contactFormData['Message'],
+                    'text/plain');
+            $mailer->send($message);
 
-        try {
-          //Server settings
-          // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-          $mail->isSMTP();                                            // Send using SMTP
-          $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-          $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-          $mail->Username   = $EmailSend;                     // SMTP username
-          $mail->Password   = 'PlayGmail12$g';                               // SMTP password
-          $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-          $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-          //Recipients
-          $mail->setFrom($fromEmail, $fromName);
-          $mail->addAddress($EmailReceive, 'Matheo Zeller');     // Add a recipient
-          $mail->addReplyTo($fromEmail, 'First Last');
-          // Content
-          $mail->isHTML(true);                                  // Set email format to HTML
-          $mail->Subject = $subject;
-          $mail->Body    = $message;
-          $mail->AltBody = $message;
 
-          $mail->send();
-          $this->session->set('contact', 'Votre message a bien été envoyé');
-          var_dump($_SESSION);
-          header('Location: /index.php');
-        } catch (Exception $e) {
-          // echo "Le message n'a pu être envoyé. Mailer Error: {$mail->ErrorInfo}";
-          $this->session->set('contact', 'Un problème est survenu');
+
+            $this->addFlash('success', 'Your message has been sent');
+
+            return $this->redirectToRoute('contact');
         }
-      }
 
-      return  $this->view->render('home', [
-        'post' => $post,
-        'errors' => $errors,
-        'articles' => $articles
-      ]);
+
+
+        return $this->render('contact/index.html.twig', [
+            'our_form' => $form->createView()
+        ]);
     }
-
-    return $this->view->render('home', [
-      'articles' => $articles
-    ]);
-  }
 }
