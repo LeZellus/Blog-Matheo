1f4387ff351d733f7724778a183114b2f87b7169
Zeller <matheo.zeller@gmail.com>
2021-01-18 11:13:45 +0100

    feat(contact): contact form method added

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,20 +3,49 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class HomeController extends AbstractController
 {
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $contactFormData = $form->getData();
+
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('matheo.zeller@gmail.com')
+                ->subject('Vous avez reçu un mail d\'un visiteur du blog')
+                ->text('Destinataire : '.$contactFormData['email'].\PHP_EOL.
+                    $contactFormData['message'],
+                    'text/plain');
+
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Your message has been sent');
+
+            return $this->redirectToRoute('home');
+        }
+
         $articles = $this->getDoctrine()->getRepository(Article::class)->findBy(
             ['isPublished' => true],
             ['publishedAt' => 'desc']
         );
 
         return $this->render('home/index.html.twig', [
-            'articles' => $articles
+            'articles' => $articles,
+            'form' => $form->createView()
         ]);
     }
 }
