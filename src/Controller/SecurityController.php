aa3cfc808451f90108a0a977caef57ce36f0308d
Zelller <matheo.zeller@gmail.com>
2021-02-20 15:47:16 +0100

    feat(password): add feature to edit password on profil page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,16 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Form\UpdatePasswordType;
 
 class SecurityController extends AbstractController
 {
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -25,5 +28,44 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    public function editPassword(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute("app_login");
+        }
+
+        $form = $this->createForm(UpdatePasswordType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $oldPassword = $request->request->get('update_password')['oldPassword'];
+            $newPassword = $request->request->get('update_password')['password']['first'];
+
+            // if the old password is valid
+            if ($passwordEncoder->isPasswordValid($user, $oldPassword)) {
+
+                $newEncodedPassword = $passwordEncoder->encodePassword($user, $newPassword);
+                $user->setPassword($newEncodedPassword);
+                $em->persist($user);
+                $em->flush();
+                $this->addFlash('success', 'Votre mot de passe à bien été changé !');
+
+                return $this->redirectToRoute('profil');
+
+            } else {
+
+            }
+        }
+
+        return $this->render('profil/edit_password.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
     }
 }
---
+++ src/Form/UpdatePasswordType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UpdatePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'required' => true,
+                'mapped'=> false,
+            ))
+            ->add('oldPassword', PasswordType::class, array(
+                'mapped' => false,
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
