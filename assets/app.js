3b813a5ce8b39260c3011ce0ae64ba9f76a3c772
Zelller <matheo.zeller@gmail.com>
2021-02-20 15:59:15 +0100

    fix(javascript): add javascript in assets for better managment

--- assets/app.js
+++ assets/app.js
@@ -11,3 +11,117 @@ import "./sass/application.scss";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+
+document.addEventListener("DOMContentLoaded", function () {
+    let articleContent = document.getElementsByClassName("article-content-input");
+    let menu = document.getElementById("nav-burger");
+    let navLinks = document.getElementById("nav-link-items");
+
+    //let cvWrapper = document.getElementById("cv-wrapper");
+    let cvImg = document.getElementById("cv-img");
+
+    /*******************************************************/
+    /**********************TinyCME**************************/
+    /*******************************************************/
+
+    //Init TinyCME only if class exist on the DOM to block error
+    if (articleContent.length > 0) {
+        tinymce.init({
+            selector: ".article-content-input",
+            plugins: "link",
+        });
+    }
+
+    /*******************************************************/
+    /**********************Popup Img************************/
+    /*******************************************************/
+
+    //Start event on click
+    let popupManage = function popup() {
+        cvImg.classList.toggle("opened");
+    };
+
+    /*******************************************************/
+    /********************CURSOR TRAIL***********************/
+    /*******************************************************/
+
+    //Color array for cursor trail
+    let colorArray = [
+        "rgba(49,226,194,1)",
+        "rgba(49,226,194,0.7)",
+        "rgba(49,226,194,0.5)",
+        "rgba(49,226,194,0.3)",
+        "rgba(49,226,194,0.1)",
+        "rgba(40,40,40,1)",
+        "rgba(40,40,40,0,7)",
+        "rgba(40,40,40,0,5)",
+        "rgba(40,40,40,0.3)",
+        "rgba(40,40,40,0.1)",
+    ];
+
+    //This function is like jQuery fadeOut
+    function fadeOut(item) {
+        let fadeEffect = setInterval(function () {
+            if (!item.style.opacity) {
+                item.style.opacity = 1;
+            }
+            if (item.style.opacity > 0) {
+                item.style.opacity -= 0.1;
+            } else {
+                clearInterval(fadeEffect);
+            }
+        }, 300);
+    }
+
+    //Calculs and properties for cursor trail style
+    let mouseMonitor = function (e) {
+        let item = document.createElement("span");
+        item.classList.add("trail");
+
+        //letiable color for trail
+        let curColor =
+            colorArray[Math.floor(Math.random() * colorArray.length)];
+
+        //Calcul cursor dimensions
+        let width = (height = Math.random() * (8 - 4) + 4);
+
+        //Calcul cursor position
+        let curX = e.pageX + Math.round(Math.random() * 10);
+        let curY = e.pageY + Math.round(Math.random() * 10);
+        let left = curX.toString();
+        let top = curY.toString();
+
+        //Add css properties defined by position
+        item.style.left = left + "px";
+        item.style.top = top + "px";
+        item.style.width = width + "px";
+        item.style.height = height + "px";
+        item.style.backgroundColor = curColor;
+
+        document.body.appendChild(item);
+        setTimeout(function () {
+            fadeOut(item);
+            item.remove();
+        }, 300);
+    };
+
+    /*******************************************************/
+    /*********************BURGER MENU***********************/
+    /*******************************************************/
+
+    //Toggle menu function
+    let toggle = function () {
+        navLinks.classList.toggle("show");
+        menu.classList.toggle("clicked");
+    };
+
+    /*******************************************************/
+    /***************LAUNCH FUNCTIONS ON LOAD****************/
+    /*******************************************************/
+    window.onload = function () {
+        this.addEventListener("mousemove", mouseMonitor);
+        menu.addEventListener("click", toggle);
+        //cvWrapper.addEventListener("click", popupManage);
+        //cvImg.addEventListener("click", popupManage);
+    };
+});
