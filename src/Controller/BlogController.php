fd0000aec670f4d176c6e9c9dd6cc978d70d3ace
Zeller <matheo.zeller@gmail.com>
2021-03-10 16:40:38 +0100

    fix(app)

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -113,7 +113,6 @@ class BlogController extends AbstractController
 
                 $article->setThumb($fileName);
             } else {
-                dump("je suis passé ici");
                 $article->setThumb($oldPicture);
             }
 
@@ -124,9 +123,6 @@ class BlogController extends AbstractController
             $this->addFlash('success', 'L\'article a bien été modifié');
             return $this->redirectToRoute("app_admin");
         }
-
-        dump($article);
-        dump($oldPicture);
 
         return $this->render('blog/edit.html.twig', [
             'article' => $article,
